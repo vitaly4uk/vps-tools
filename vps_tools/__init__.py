c8461149da331248da4c477ace0ac4b6f0140962
vitalii omelchuk <vitaly.omelchuk@gmail.com>
2017-09-24 15:20:03 +0300

    posgres dump/restore fix

--- vps_tools/__init__.py
+++ vps_tools/__init__.py
@@ -1,1 +1,1 @@
-VERSION="0.2.8a4"
+VERSION="0.2.8a5"
--- vps_tools/pg.py
+++ vps_tools/pg.py
@@ -15,8 +15,8 @@ def dump(project_name, dump):
     home_folder = '/home/{project_name}'.format(project_name=project_name)
     with cd(home_folder), settings(sudo_user='postgres'), shell_env(HOME=home_folder), hide('output'):
         with StreamFilter([database['PASSWORD']], sys.stdout):
-            sudo('PGPASSWORD={PASSWORD} pg_dump -Fc --no-acl --no-owner {NAME} > latest.dump'.format(**database))
-        get('latest.dump', dump, temp_dir='/tmp')
+            sudo('PGPASSWORD={PASSWORD} pg_dump -Fc --no-acl --no-owner {NAME} > /tmp/{NAME}_latest.dump'.format(**database))
+        get('/tmp/{NAME}_latest.dump'.format(**database), dump)
 
 
 @task
@@ -26,10 +26,10 @@ def restore(project_name, dump):
     home_folder = '/home/{project_name}'.format(project_name=project_name)
     sudo('supervisorctl stop {project_name}'.format(project_name=project_name))
     with cd(home_folder), settings(sudo_user='postgres'), shell_env(HOME=home_folder):
-        put(dump, '/tmp/{project_name}.dump'.format(project_name=project_name))
+        put(dump, '/tmp/{NAME}_latest.dump'.format(**database))
         with settings(sudo_user='postgres'):
             sudo('dropdb --if-exists -h {HOST} -p {PORT} {NAME}'.format(**database))
             sudo('createdb {NAME} -O {USER} -h {HOST} -p {PORT}'.format(**database))
         with hide('output'), settings(warn_only=True), StreamFilter([database['PASSWORD']], sys.stdout):
-            sudo('PGPASSWORD={PASSWORD} pg_restore --clean --no-acl --no-owner -d {NAME} /tmp/latest.dump'.format(**database))
+            sudo('PGPASSWORD={PASSWORD} pg_restore --clean --no-acl --no-owner -d {NAME} /tmp/{NAME}_latest.dump'.format(**database))
     sudo('supervisorctl start {project_name}'.format(project_name=project_name))
